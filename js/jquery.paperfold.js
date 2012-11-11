eaccc36d5c1d5927427902c91f18af9e4171bccf
Dmitry Kharchenko <unclenorton@gmail.com>
2012-11-12 00:35:34 +0800

    Initial commit

---
+++ js/jquery.paperfold.js
@@ -0,0 +1,250 @@
+var transformString = Modernizr.prefixed('transform');
+
+var transEndEventNames = {
+	'WebkitTransition': 'webkitTransitionEnd',
+	'MozTransition': 'transitionend',
+	'OTransition': 'oTransitionEnd',
+	'msTransition': 'MSTransitionEnd',
+	'transition': 'transitionend'
+},
+	transEndEventName = transEndEventNames[Modernizr.prefixed('transition')];
+
+
+// replace jQuery.fx.step._default
+var $_fx_step_default = $.fx.step._default;
+$.fx.step._default = function(fx) {
+	if(!fx.elem.customAnimate) return $_fx_step_default(fx);
+	fx.elem[fx.prop] = fx.now;
+	fx.elem.updated = true;
+};
+
+
+var paperfold = {
+	percentage: 0,
+
+	init: function(element, maxHeight, toggleCallback) {
+		this.element = $(element);
+		this.maxHeight = maxHeight;
+		this.toggleCallback = toggleCallback;
+
+		// get real element height
+		this.height = this.element.css('height', 'auto').outerHeight();
+		this.element.css('height', '');
+
+		// calculate amount and height of the folds
+		this.foldCount = Math.ceil(this.height / this.maxHeight);
+		this.foldHeight = Math.floor(this.height / this.foldCount);
+
+		// Make sure it is an even number, so we can split in in 2
+		// this.foldHeight = (this.foldHeight % 2 === 0) ? this.foldHeight : this.foldHeight + 1;
+		// detach the elements children from the dom and cache them 
+		this.content = this.element.children().detach();
+
+		// add folds containing the previously cached children elements
+		// to the element
+		for(var i = 0, j = 0; i < this.foldCount; i++, j += 2) {
+			var topHeight = bottomHeight = Math.ceil(this.foldHeight / 2);
+
+			if((i + 1) === this.foldCount && this.foldHeight / 2 % 2) bottomHeight = this.height - (j + 1) * topHeight + 2;
+
+			this.element.append(this.createFold(j, topHeight, bottomHeight));
+		}
+
+		// cache the folds -> can i do this while creating them?
+		// i mean i can of course cache them but then the dom connection is not there
+		// i'd love to get a hint: @mrflix or mrflix@gmail.com
+		this.folds = this.element.find('> .pf__fold');
+		this.bottoms = this.folds.find('> .pf__bottom');
+		this.tops = this.folds.find('> .pf__top');
+
+		// bind buttons
+		this.trigger = this.element.prev('.event__trigger');
+		this.trigger.click($.proxy(this, 'toggle'));
+
+		this.element.addClass('pf__ready');
+	},
+	update: function(maxHeight) {
+		this.element.children().detach();
+		this.element.html(this.content);
+		this.init(this.element, maxHeight);
+		if(this.percentage !== 0) {
+			this.open(this.percentage);
+		}
+	},
+	createFold: function(j, topHeight, bottomHeight) {
+		var offsetTop = -j * topHeight;
+		var offsetBottom = -this.height + j * topHeight + this.foldHeight;
+		return $('<div>').addClass('pf__fold').append(
+		$('<div>').addClass('pf__top').css('height', topHeight).append(
+		$('<div>').addClass('pf__wrapper').append(
+		$('<div>').addClass('pf__inner').css('top', offsetTop).append(this.content.clone()))).add($('<div>').addClass('pf__bottom').css('height', bottomHeight).append(
+		$('<div>').addClass('pf__wrapper').append(
+		$('<div>').addClass('pf__inner').css('bottom', offsetBottom).append(this.content.clone())))));
+	},
+	toggle: function() {
+
+		this.element.toggleClass('pf__visible');
+
+		if(this.element.hasClass('pf__visible')) {
+			// open
+			// animate folds height (css transition)
+			this.folds.height(this.foldHeight);
+			this.trigger.removeClass('pf__trigger_collapsed').addClass('pf__trigger_expanded');
+		} else {
+			// close
+			// animate folds height (css transition)
+			this.folds.height(0);
+			this.trigger.removeClass('pf__trigger_expanded').addClass('pf__trigger_collapsed');
+		}
+		this.tops.add(this.bottoms).css('background-color', '').css(transformString, '');
+	},
+	open: function(percentage) {
+		// cache percentage
+		this.percentage = percentage;
+
+		// change angle of tops and bottoms
+		var c = this.foldHeight * percentage,
+			a = b = this.foldHeight / 2,
+			part = 2 * b * c,
+			bottomAngle = part <= 0 ? 90 : Math.acos((b * b + c * c - a * a) / part) * 180 / Math.PI,
+			topAngle = 360 - bottomAngle;
+
+		this.tops.css(transformString, 'rotateX(' + topAngle + 'deg)');
+		this.bottoms.css(transformString, 'rotateX(' + bottomAngle + 'deg)');
+
+		// change folds height
+		var foldHeight = this.height / this.foldCount * percentage;
+		this.folds.height(foldHeight);
+
+		// change the background color
+		// from dark hsl(192,6,33) at 0
+		// to light hsl(192,0,100) at 100
+		var saturation = Math.round(6 - 6 * percentage),
+			lightness = 33 + Math.round(67 * percentage),
+			backgroundColor = 'hsl(192,' + saturation + '%,' + lightness + '%)';
+
+		this.tops.add(this.bottoms).css('background-color', backgroundColor);
+	},
+};
+
+
+$.eventBus.bind('paperfold init', function(e) {
+	// $(window).load(function (e) {
+	var paperfolds = [];
+	var hiddenElements = $('.pf__hidden');
+
+	if(Modernizr.csstransforms3d && !jia.isMobileBrowser) {
+		$(hiddenElements).addClass('pf__inited');
+		$.each(hiddenElements, function(i, element) {
+			paperfolds[i] = Object.create(paperfold);
+			paperfolds[i].init(element, 250);
+		});
+	}
+
+	$('.pf__trigger').show();
+
+});
+
+/**
+ * @author Dmitry Kharchenko (dmitry@upfrontmedia.asia)
+ * @version 0.1
+ * @date 2012-11-12
+ * @requires jQuery 1.7.2
+ *
+ * A jquery plugin for paperfold effect (as seen at https://developer.mozilla.org/en-US/demos/detail/paperfold-css/launch)
+ *
+ */
+
+(function($) {
+	"use strict";
+
+	$.paperfold = $.paperfold || {
+		version: '1.0'
+	};
+
+	//Default parameters
+	$.paperfold.conf = {
+		duration: 500,
+		foldHeight: 250,
+		items: '.pf__item'
+	};
+
+	$.paperfold.isMobileBrowser = (navigator.userAgent.match(/Android/i) ||
+			 navigator.userAgent.match(/webOS/i) ||
+			 navigator.userAgent.match(/iPhone/i) ||
+			 navigator.userAgent.match(/iPod/i) ||
+			 navigator.userAgent.match(/iPad/i) ||
+			 navigator.userAgent.match(/BlackBerry/)
+			 );
+
+	$.fn.paperfold = function(conf) {
+
+		//Extend defaults
+		conf = $.extend($.iGlow.paperfold, conf);
+
+		var jItems = this,
+				paperfolds = [];
+
+		jItems.each(function(i, el) {
+			if(Modernizr.csstransforms3d && !$.paperfold.isMobileBrowser) {
+				el.addClass('pf__inited');
+				paperfolds[i] = Object.create(paperfold);
+				paperfolds[i].init(el, conf.foldHeight);
+			} else {
+				console.log('not supported');
+			}
+		});
+
+		return this;
+	};
+
+})(jQuery);
+
+
+//Expander object. Example usage: 'read more' in front page sections
+var Expander = function (options) {
+	var defaults = {
+		expandable : '.g-expandable',
+		expanded : '.g-expanded',
+		trigger : '.g-trigger',
+		pseudo : '.g-pseudo',
+		
+		expandedClass : 'g-trigger_expanded',
+		collapsedClass : 'g-trigger_collapsed'
+	};
+
+	this.options = $.extend({}, defaults, options ? options : {});
+
+	this.jExpandable = $(this.options.expandable);
+	
+	this.expandedClass = this.options.expanded;
+	
+	this.collapsedTrigger = this.options.collapsedClass;
+	this.expandedTrigger = this.options.expandedClass;
+	
+	this.trigger = this.options.trigger;
+	
+	this.jTriggers = $(this.trigger, this.jExpandable);
+	var _self = this;
+
+	this.jTriggers.live('click', function (e) {
+		var jCurrent = $(this),
+			expander = true;
+		
+		
+		// Check current status
+		expander = (jCurrent.hasClass(_self.collapsedTrigger));
+		var jFull = jCurrent.siblings(_self.expandedClass);
+
+		if (!jFull.is(':animated')) {	
+			if (expander) { // expand
+				jCurrent.removeClass(_self.collapsedTrigger).addClass(_self.expandedTrigger);
+				jFull.slideDown(300);
+			} else { // collapse
+				jCurrent.removeClass(_self.expandedTrigger).addClass(_self.collapsedTrigger);
+				jFull.slideUp(300);
+			}
+		}
+		
+	});
+};
---
+++ readme.md
@@ -0,0 +1,15 @@
+# jQuery Paperfold
+A jQuery wrapper for the paperfold effect featured on MDN demo page: https://developer.mozilla.org/en-US/demos/detail/paperfold-css/launch
+
+# Usage
+
+# Options
+
+
+# Compatibility
+
+
+
+# License
+
+This plugin is released under MIT license and distributed *'as is'*, without any warranty, explicit or implied.
